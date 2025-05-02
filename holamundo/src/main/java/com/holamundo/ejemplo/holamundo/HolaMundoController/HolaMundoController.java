f44625e1b129176adb4bb1a71998e6151c3942a4
Alonso Diaz <alo.diaz@duocuc.cl>
2025-05-02 16:21:42 -0400

    Add files via upload

---
+++ holamundo/src/main/java/com/holamundo/ejemplo/holamundo/HolaMundoController/HolaMundoController.java
@@ -0,0 +1,14 @@
+package com.holamundo.ejemplo.holamundo.HolaMundoController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String holaMundo() {
+        return "HOLA MUNDOO";
+    }
+
+}
